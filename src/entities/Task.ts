606380d286cd33869a314845660681af103eb1cc
unknown <jvalves217@gmail.com>
2023-12-08 22:11:06 -0300

    ]

--- src/entities/Task.ts
+++ src/entities/Task.ts
@@ -1,25 +1,24 @@
 class Task {
-    id?: string;
-    title: string;
-    description: string;
-    categories: string;
-    status: number;
-    authorId: string;
+  id?: string;
+  title: string;
+  description: string;
+  categories: string;
+  status: number;
+  authorId: string;
+
+  private constructor({  title, description, categories, status, authorId }: Task) {
+      this.title = title;
+      this.description = description;
+      this.categories = categories;
+      this.status = status;
+      this.authorId = authorId;
+    };
   
-    private constructor({  title, description, categories, status, authorId }: Task) {
-      return Object.assign(this, {
-        title, 
-        description, 
-        categories, 
-        status, 
-        authorId
-      });
-    }
-  
-    static create({ title, description, categories, status, authorId }: Task) {
-      const user = new Task({ title, description, categories, status, authorId });
-      return user;
-    }
+
+  static create({ title, description, categories, status, authorId }: Task) {
+    const user = new Task({ title, description, categories, status, authorId });
+    return user;
   }
-  
-  export { Task };
+}
+
+export { Task };
